390265abaa34832aa85a1756f3bcbf5a87591622
Josh <josh910830@gmail.com>
2023-05-17 19:51:31 +0900

    initial commit

---
+++ src/main/java/com/github/josh910830/portablemqdemo/PortableMqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.josh910830.portablemqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortableMqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortableMqDemoApplication.class, args);
+    }
+
+}
